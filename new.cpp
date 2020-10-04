df445fd86c8dc2c6912e0e0b761b0e7c46d7eb00
suprithu <suprithum@gmail.com>
2020-10-04 05:16:56 +0000

    new

---
+++ new.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	float a, b, c;
+	cout<<"\nEnter the numbe\n\n"<<endl;
+	cin>>a>>b;
+	c = a + b;
+        cout<<"\nThe Addition is\n"<< c << endl;
+	return 0;
+}	
